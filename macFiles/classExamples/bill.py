f756ff6934eac331b364c709bc74ffd768564010
Shawn Conboy <conboysbusiness@gmail.com>
2025-05-20 12:23:22 -0400

    init files

---
+++ macFiles/classExamples/bill.py
@@ -0,0 +1,28 @@
+# this program will calculate tips at 15 and 20 %
+# users enters amount of their bill
+
+# this program was written by Shawn Conboy on May 20th 2025
+
+import time
+
+bill = input('Please enter your bill amount : ')
+
+# calculate tip amounts
+
+tip15 = float(bill) * 0.15          # This is the 15% tip
+
+tip20 = float(bill) * 0.20          # This is the 20% tip
+
+amount15 = tip15 + float(bill)
+
+amount20 = tip20 + float(bill)
+
+# Display results
+
+print()
+
+print ('The amount of your tip if you tip 15% is $' + format(tip15, ',.2f') + ' And the total amount is ' +  format(amount15, ',.2f'))
+
+print()
+
+time.sleep(5)
---
+++ macFiles/weeklyAssignments/tuitionIdle.py
@@ -0,0 +1,27 @@
+# This program will ask a user for their name, total credit hours being taken and calculate their total tuition cost.
+# This program was made by Shawn Conboy on May 20th 2025
+
+# This will allow for delay prior to program closing so user can see output.
+import time
+
+
+# Program begins here.
+print()
+print()
+
+# Ask user for name
+name = input('Please enter your name : ') 
+
+# Assing tuition value, and ask for total credit hours
+tuition = 198
+credithours = input('How many credit hours are you taking? : ')
+
+
+# Calculate tuition cost by converting credit hours to an int to do arithmetic
+totalPrice = (tuition * (int(credithours)))
+
+# Display output showing summary of program
+print()
+print('Hi ' + name + '. Your tuition is $' + format(totalPrice, ',.2f') )
+
+time.sleep(5)
