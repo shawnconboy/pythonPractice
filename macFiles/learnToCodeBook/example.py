04ea134c1f73a80502605fc7259054e34c3c3847
Shawn Conboy <conboysbusiness@gmail.com>
2025-05-29 15:58:35 -0400

    init files

---
+++ macFiles/learnToCodeBook/example.py
@@ -0,0 +1,5 @@
+yearOfBirth = 1990
+peopleInRoom = 20
+name = "mary"
+
+print(f"Hello {name.capitalize()}, you were born in {str(yearOfBirth)} and there are {str(peopleInRoom)} people in the room.")
